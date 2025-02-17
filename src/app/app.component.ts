9d0ca857f995dec6d7dd916b93ee085909a9d53c
lucasbarbosadev <lucasbarbosadev1@gmail.com>
2025-02-16 23:12:33 -0300

    docs/feat: Projeto angular iniciado/Header finalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/Home/header/header.component";
+import { AboutUsComponent } from "./components/Home/about-us/about-us.component";
+import { CatalogComponent } from "./components/Home/categories/catalog.component";
+import { SocialNetworksComponent } from "./components/Home/social-networks/social-networks.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, AboutUsComponent, CatalogComponent, SocialNetworksComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/Home/social-networks/social-networks.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-social-networks',
+  imports: [],
+  templateUrl: './social-networks.component.html',
+  styleUrl: './social-networks.component.scss'
+})
+export class SocialNetworksComponent {
+
+}
