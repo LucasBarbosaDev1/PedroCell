a7dddfd1927a932e27373e42913425f4b012a507
lucasbarbosadev <lucasbarbosadev1@gmail.com>
2025-02-17 15:18:45 -0300

    feat: section 'categories' finalizado

--- src/app/components/Home/categories/catalog.component.ts
+++ src/app/components/Home/categories/catalog.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import  { routes } from '../../../app.routes'
 
 @Component({
   selector: 'app-catalog',
-  imports: [],
+  imports: [  ],
   templateUrl: './catalog.component.html',
   styleUrl: './catalog.component.scss'
 })
-export class CatalogComponent {
 
+export class CatalogComponent {
 }
+
